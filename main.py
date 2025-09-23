a5b32403c93f71557d8ed85a346c582eae0f45b9
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-23 10:53:51 +0800

    3_B: Print Test Cases

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-pair = list(map(int, input().split(" ")))
-pair = sorted(pair)
-ans = ""
-for i in range(len(pair)):
-    if i != len(pair) - 1:
-        ans += str(pair[i]) + " "
-    else:
-        ans += str(pair[i])
-print(ans)
+for i in range(10000):
+    user_input = input()
+    num = int(user_input)
+    if num == 0:
+        break
+    print(f"Case {i+1}: {num}")
