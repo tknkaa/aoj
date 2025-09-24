731a40bb0b75c5e76ee41bc11a85d08990304f31
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-24 10:29:09 +0800

    Bubble Sort

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-ints = list(map(int, input().split(" ")))
-a = ints[0]
-b = ints[1]
-if a <= b:
-    tmp = a
-    a = b
-    b = tmp
-r = a%b
-while r != 0:
-    a = b
-    b = r
-    r = a%b
+n = int(input())
+a = list(map(int, input().split(" ")))
+count = 0
+for i in range(n):
+    for j in range(n - 1, i, -1):
+        if a[j] < a[j-1]:
+            count += 1
+            tmp = a[j]
+            a[j] = a[j-1]
+            a[j-1] = tmp
 
-print(b)
+output = ""
+for i in range(n):
+    output += str(a[i])
+    if i != n - 1:
+        output += " "
+print(output)
+print(count)
